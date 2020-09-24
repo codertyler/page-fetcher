33bcd3b07db3d0249e5482a437b68bdb427a22c8
codertyler <codertylerasai@gmail.com>
2020-09-24 04:06:34 +0000

    Completed file

--- fetcher.js
+++ fetcher.js
@@ -1,12 +1,31 @@
 let urlInput = process.argv[2];
 let filePath = process.argv[3];
 
-console.log(urlInput);
+// console.log(urlInput);
 
 const fs = require('fs');
-const reqeust = require('request');
+const request = require('request');
 
-fs.writeFile(`./${filePath}`);
+// fs.writeFile(`./${filePath}`);
+
+request(`${urlInput}`, function(error, response, body){
+  
+  
+
+    const writingFile = fs.writeFile(`${filePath}`, body, (err) => {
+      if (err) process.exit();
+      
+      let stats = fs.statSync(`${filePath}`);
+      let bytes = stats.size;
+       
+
+
+      console.log(`Downloaded and saved ${bytes} bytes in ${filePath}`);
+  })
+
+
+})
+  
 
 
 
